aa2f8114aea893a24c04b3c6200af8b20da7afae
mandersson1024 <mixand@gmail.com>
2019-02-26 21:44:06 +0100

    Working on tile shader.

--- Assets/ASCII/Shaders/Texture2DArrayCreator.cs
+++ Assets/ASCII/Shaders/Texture2DArrayCreator.cs
@@ -9,8 +9,8 @@ public class Texture2DArrayCreator : MonoBehaviour
     [MenuItem("ASCII/Create Luminosity Texture2DArray")]
     static void CreateTexture2DArray()
     {
-        const int depth = 256;
-        const int tileSize = 32;
+        const int depth = 5;
+        const int tileSize = 40;
 
         Texture2DArray array = new Texture2DArray(tileSize, tileSize, depth, TextureFormat.RGB24, false);
 
@@ -19,8 +19,8 @@ public class Texture2DArrayCreator : MonoBehaviour
             Color[] pixels = new Color[tileSize * tileSize];
             for (int j = 0; j < tileSize * tileSize; ++j)
             {
-                int value = i * (256 / depth);
-                Color32 col = new Color32((byte)i, (byte)i, (byte)i, 0);
+                byte value = (byte) ((i * 256) / depth);
+                Color32 col = new Color32(value, value, value, 0);
                 pixels[j] = col;
             }
             
@@ -30,5 +30,24 @@ public class Texture2DArrayCreator : MonoBehaviour
         array.Apply();
         AssetDatabase.CreateAsset(array, "Assets/ASCII/Shaders/Luminosity Texture2D Array.asset");
     }
+
+    [MenuItem("ASCII/Create Texture2DArray From Spritesheet")]
+    static void CreateTexture2DArrayFromSpriteSheet()
+    {
+        Object[] selection = Selection.objects;
+        Texture2D[] textures = new Texture2D[selection.Length];
+        for (int i = 0; i < textures.Length; i++)
+        {
+            textures[i] = (Texture2D)selection[i];
+        }
+
+        Texture2DArray array = new Texture2DArray(textures[0].width, textures[0].height, textures.Length, textures[0].format, false);
+        for (int i = 0; i < textures.Length; i++)
+            array.SetPixels(textures[i].GetPixels(), i);
+
+        array.Apply();
+        AssetDatabase.CreateAsset(array, "Assets/ASCII/Shaders/Spritesheet Texture2D Array.asset");
+    }
+
 
 }

40c2940b83bd05ecb523ce048a8b0eeecf5bde3c
mandersson1024 <mixand@gmail.com>
2019-03-09 22:36:30 +0100

    Working on MapTextureFactory

---
+++ Assets/Teaser Trailer/Map.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Kalaskod
+{
+    public class Map : MonoBehaviour
+    {
+        const int width = 64;
+        const int height = 36;
+        const int tileSize = 40;
+
+        Tileset tileset;
+        readonly SpriteRenderer[,] renderers = new SpriteRenderer[width, height];
+
+        void Start()
+        {
+            tileset = new Tileset("Textures/Spritesheet");
+
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    renderers[x, y] = CreateRendererAt(x, y);
+                }
+            }
+
+            PopulateFromCharacterMap(CharacterMapper.sampleCharacterMapSource);
+        }
+
+        private SpriteRenderer CreateRendererAt(int x, int y)
+        {
+            string name = "Tile[" + x + "," + y + "]";
+            GameObject go = new GameObject(name);
+            SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+            SetPosition(x, y, sr);
+            return sr;
+        }
+
+        private void SetPosition(int x, int y, SpriteRenderer rend)
+        {
+            float _x = x - (width / 2) + 0.5f;
+            float _y = -y + (height / 2) - 0.5f;
+            rend.transform.localPosition = new Vector3(_x, _y, 0f);
+        }
+
+        private void PopulateFromCharacterMap(string characterMap)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                for (int x = 0; x < width; ++x)
+                {
+                    char c = characterMap[y*width + x];
+                    int index = CharacterMapper.GetIndex(c);
+                    Sprite sprite = tileset.GetTile(index);
+                    renderers[x,y].sprite = sprite;
+                }
+            }
+        }
+
+        void Update()
+        {
+            //StressTest.Do(tileset, width, height, renderers);
+        }
+
+    }
+}
+
+
---
+++ Assets/Teaser Trailer/Tileset.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalaskod
+{
+    public class Tileset
+    {
+        readonly Dictionary<char, Texture2D> tiles = new Dictionary<char, Texture2D>();
+        Sprite[] sprites;
+
+        public Tileset(string spritesheetName)
+        {
+            sprites = Resources.LoadAll<Sprite>(spritesheetName);
+            /* 
+             * todo: load json file that contains names and other data. Possibly create several ways of accessing them.
+             * 
+             *  {
+             *      [
+             *          {
+             *              "id": 0,
+             *              "symbol": "#",
+             *              "luminosity": 0.8,
+             *              "pixmap": "#ff00ff"
+             *          },
+             *          {
+             *              "id": 1
+             *              ...
+             *          }
+             *      ]
+             *  }
+             */
+        }
+
+        void AddTile(char chr, Texture2D tex)
+        {
+            tiles[chr] = tex;
+        }
+
+        public Texture2D GetTile(char chr)
+        {
+            return tiles[chr];
+        }
+
+        public Sprite GetTile(int index)
+        {
+            return sprites[index];
+        }
+
+        public int Length
+        {
+            get { return sprites.Length; }
+        }
+    }
+}

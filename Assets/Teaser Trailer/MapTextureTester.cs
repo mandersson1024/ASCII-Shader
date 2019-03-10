96c5ee84b5464fc89bbea81b889b659f0ec5d9be
mandersson1024 <mixand@gmail.com>
2019-03-10 13:30:33 +0100

    Tested creating a tilemap by drawing tiles onto a texture.

--- Assets/Teaser Trailer/MapTextureTester.cs
+++ Assets/Teaser Trailer/MapTextureTester.cs
@@ -4,39 +4,64 @@ using UnityEngine;
 
 public class MapTextureTester : MonoBehaviour
 {
-    public Texture2D[] tiles;
+    readonly int pixelsPerUnit = 30;
+
+    public Color[] colors;
+    Texture2D texture;
+    Texture2D tile;
 
     // Start is called before the first frame update
     void Start()
     {
-        var spriteRenderer = GetComponent<SpriteRenderer>();
-        Texture2D texture = new Texture2D(1920, 1080, TextureFormat.ARGB32, false);
+        texture = new Texture2D(1920, 1080, TextureFormat.ARGB32, false);
+        tile = new Texture2D(pixelsPerUnit, pixelsPerUnit, TextureFormat.ARGB32, false);
+
         texture.FillWithColor(new Color32(255, 0, 255, 255));
 
-        var sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), new Vector2(0f, 0f), 30);
-        spriteRenderer.sprite = sprite;
+        var sprite = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), pixelsPerUnit);
+        GetComponent<SpriteRenderer>().sprite = sprite;
 
-
-
-        //Graphics.CopyTexture(tiles[0], texture);
-
-        /*
         int numTilesX = 64;
         int numTilesY = 36;
+        int colorIndex = 0;
 
         for (int y = 0; y < numTilesY; ++y)
         {
-            for (int x = 0; x < numTilesY; ++x)
+            for (int x = 0; x < numTilesX; ++x)
             {
+                tile.FillWithColor(colors[colorIndex]);
 
+                CopyTexture(tile, texture, x * pixelsPerUnit, y * pixelsPerUnit);
+                ++colorIndex;
+                colorIndex %= colors.Length;
             }
         }
-        */
+    }
+
+    void CopyTexture(Texture2D src, Texture2D dst, int x, int y)
+    {
+        Graphics.CopyTexture(src, 0, 0, 0, 0, src.width, src.height, dst, 0, 0, x, y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        int numTilesX = 64;
+        int numTilesY = 36;
+        int colorIndex = 0;
+
+        for (int y = 0; y < numTilesY; ++y)
+        {
+            for (int x = 0; x < numTilesX; ++x)
+            {
+                tile.FillWithColor(colors[colorIndex]);
+
+                CopyTexture(tile, texture, x * 30, y * 30);
+                ++colorIndex;
+                colorIndex %= colors.Length;
+            }
+        }
+        */
     }
 }
